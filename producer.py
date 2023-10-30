6887fa47eab6a311a4874850329201d225c39a1b
root <root@Fidelis>
2023-10-30 13:30:36 +0100

    Producer-many messages

--- producer.py
+++ producer.py
@@ -1,4 +1,4 @@
-from confluent_kafka import Producer, KafkaError
+from confluent_kafka import Producer
 import requests
 import json
 
@@ -6,12 +6,15 @@ import json
 SERVER_NAME = 'localhost:9092'
 # Définir le nom du Topic
 TOPIC_NAME = 'users_profiles'
+# Définir le nombre de messages à envoyer
+NUM_MESSAGES = 10
 
 # Configuration du producteur Kafka
 config = {'bootstrap.servers': SERVER_NAME}
 
 producer = Producer(config)
 
+# fonction de rapport sur les d'erreurs
 def delivery_report(err, msg):
     if err is not None:
         print('Erreur lors de l\'envoi de message : {}'.format(err))
@@ -19,24 +22,26 @@ def delivery_report(err, msg):
         print('Message envoyé avec succès à {}, offset : {}'.format(msg.topic(), msg.offset()))
 
 try:
-    # Récupérer les données depuis randomuser.me
-    url = "https://randomuser.me/api/"
-    response = requests.get(url)
+    for message_num in range(1, NUM_MESSAGES + 1):
+        # Récupérer les données depuis randomuser.me
+        url = "https://randomuser.me/api/"
+        response = requests.get(url)
 
-    if response.status_code != 200:
-        print(f"Erreur HTTP : Code de statut {response.status_code}")
-    else:
-        data = response.json()
-        print(data["results"][0])
+        if response.status_code != 200:
+            print(f"Erreur HTTP : Code de statut {response.status_code}")
+        else:
+            data = response.json()
+            print(f"Message {message_num} envoyé !")  # Affiche le numéro du message envoyé
+            # print(data["results"][0])
 
-        # Sérialiser en JSON
-        json_data = json.dumps(data).encode('utf-8')
+            # Sérialiser en JSON
+            json_data = json.dumps(data).encode('utf-8')
 
-        # Envoyer les données au sujet Kafka
-        producer.produce(TOPIC_NAME, value=json_data, callback=delivery_report)
-        producer.flush()
+            # Envoyer les données au sujet Kafka
+            producer.produce(TOPIC_NAME, value=json_data, callback=delivery_report)
 
-        print("Données envoyées avec succès à Kafka")
+    producer.flush()
+    print(f"{NUM_MESSAGES} données ont été envoyées avec succès à Kafka")
 
 except requests.exceptions.RequestException as req_error:
     print("Erreur lors de la demande vers randomuser.me:", req_error)
